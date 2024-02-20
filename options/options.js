02ab659325d7699933775ab0833385ccafb43363
Chris LloydJones <chris.lloyd-jones@avanade.com>
2024-02-20 09:39:18 -0800

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "late-paper-46505",
-  url: "https://late-paper-46505.botics.co",
-  api: "https://late-paper-46505.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "late-paper-46505",
+  "url": "https://late-paper-46505.botics.co",
+  "api": "https://late-paper-46505.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
